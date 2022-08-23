7dbbd9d78d3d385c48731e236f019b7b4027c3c8
Sergei <67990404+D1esel9@users.noreply.github.com>
2022-08-23 16:06:22 +0300

    Update fullnode.sh

--- fullnode.sh
+++ fullnode.sh
@@ -3,3 +3,4 @@ mkdir ~/testnet
 cd ~/testnet
 wget -qO docker-compose.yaml https://raw.githubusercontent.com/aptos-labs/aptos-core/main/docker/compose/aptos-node/docker-compose-fullnode.yaml
 wget -qO fullnode.yaml https://raw.githubusercontent.com/aptos-labs/aptos-core/main/docker/compose/aptos-node/fullnode.yaml
+cd ~/testnet
